169863be42e2625296d309ff2411ba196af0ea42
Martinez <martil67@medtronic.com>
2025-08-04 20:11:59 -0600

    Primer commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-[#f5f5f5]">
+      <h1 className="text-5xl font-light text-[#170f5f]">
+        Hola Mundo
+      </h1>
+    </main>
+  )
+}
